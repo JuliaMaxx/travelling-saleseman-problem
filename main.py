7f7127d11f6e50e4e98135ec18185872b14c5570
Julia Max <jjuliamaxxx@gmail.com>
2024-12-18 14:17:15 +0100

    refactor: change df and indexes to constants

--- main.py
+++ main.py
@@ -20,34 +20,37 @@ def parse_tsp(filename):
         x = float(parts[1])
         y = float(parts[2])
         data.append([x, y])
-    df = pd.DataFrame(data, columns=['x', 'y'])
-    return df
+    DF = pd.DataFrame(data, columns=['x', 'y'])
+    return DF
+
+DF = parse_tsp('berlin11.tsp')
+INDEXES = DF.index.to_list()
 
 def distance_between(x1, x2, y1, y2):
     result = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     return round(result, 3)
 
-def random_solution(indexes):
-   shuffled_indexes = indexes.copy()
-   random.shuffle(shuffled_indexes)
+def random_solution():
+   shuffled_INDEXES = INDEXES.copy()
+   random.shuffle(shuffled_INDEXES)
    
    # Ensure the solution returns to the starting city
-   shuffled_indexes.append(shuffled_indexes[0])
-   return shuffled_indexes
+   shuffled_INDEXES.append(shuffled_INDEXES[0])
+   return shuffled_INDEXES
 
 def fitness(solution):
     # [9, 1, 4, 0, 10, 3, 7, 8, 6, 5, 2]
     total_distance = 0
     if len(solution) > 1:
         for i in range(len(solution) - 1):
-            # Calculate indexes
+            # Calculate INDEXES
             current_index = solution[i]
             next_index = solution[i + 1]
             
             # Calculate Distance
             distance = distance_between(
-                df.iloc[current_index]['x'], df.iloc[next_index]['x'],
-                df.iloc[current_index]['y'], df.iloc[next_index]['y']
+                DF.iloc[current_index]['x'], DF.iloc[next_index]['x'],
+                DF.iloc[current_index]['y'], DF.iloc[next_index]['y']
             )
             total_distance += distance
     return round(total_distance, 3)
@@ -62,21 +65,21 @@ def print_result(solution):
     print(f"Total Distance: {total_distance}")
     
 def greedy_solution(starting_point):
-    remaining_indexes = indexes.copy()
+    remaining_INDEXES = INDEXES.copy()
     
     # Add starting point as the fist city in the solution
     solution = [starting_point]
     
-    while len(remaining_indexes) > 1:
-        remaining_indexes.remove(starting_point)   
+    while len(remaining_INDEXES) > 1:
+        remaining_INDEXES.remove(starting_point)   
         shortest_distance = float('inf')
         next_city = -1
         
         # Calculate what is the closest city to the current one
-        for i in remaining_indexes:
+        for i in remaining_INDEXES:
             distance = distance_between(
-                df.iloc[i]['x'], df.iloc[starting_point]['x'],
-                df.iloc[i]['y'], df.iloc[starting_point]['y']
+                DF.iloc[i]['x'], DF.iloc[starting_point]['x'],
+                DF.iloc[i]['y'], DF.iloc[starting_point]['y']
                 )
             if distance < shortest_distance:
                 shortest_distance = distance
@@ -91,7 +94,7 @@ def greedy_solution(starting_point):
 def find_the_best():
     shortest = float('inf')
     city = 0
-    for i in indexes:
+    for i in INDEXES:
         distance = fitness(greedy_solution(i))
         if distance < shortest:
             shortest = distance
@@ -102,13 +105,11 @@ def find_the_best():
 def average_of_random(amount):
     total = 0
     for _ in range(amount):
-        random_sol = random_solution(indexes)
+        random_sol = random_solution()
         total += fitness(random_sol)         
     return round(total / amount, 3)      
     
-df = parse_tsp('berlin52.tsp')
-indexes = df.index.to_list()
 
-print(df)
+print(DF)
 print_result(greedy_solution(find_the_best()))
 print(average_of_random(100))
