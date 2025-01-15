52dedf5e2c916d6388fe77653f06b07a096a0340
Julia Max <jjuliamaxxx@gmail.com>
2025-01-15 16:16:59 +0100

    refector: seperate point creation from lines

--- static/index.js
+++ static/index.js
@@ -17,40 +17,58 @@ pointRange.addEventListener("input", () => {
 // Handle the points data from the backend
 socket.on('receive_points', function(data) {
     points = data.points;
-    updateCanvas();
+    updatePoints();
 });
 
-// Update the canvas with D3.js visualization
-function updateCanvas() {
-    // Clear the canvas
-    d3.select('#canvas').html('');
+// Set up the D3 canvas
+const svg = d3.select('#canvas')
+.append('svg')
+.attr('width', canvas.clientWidth)
+.attr('height', canvas.clientHeight);
 
-    // Set up the D3 canvas
-    const svg = d3.select('#canvas')
-    .append('svg')
-    .attr('width', canvas.clientWidth)
-    .attr('height', canvas.clientHeight);
+// Create groups for lines and circles
+const lineGroup = svg.append('g').attr('class', 'lines');
+const circleGroup = svg.append('g').attr('class', 'circles');
+
+function updatePoints() {
+        // Clear the circles and lines from the canvas
+        circleGroup.selectAll('circle').remove();
+        lineGroup.selectAll('path').remove();
+
+
+        // Create circles for each point
+        circleGroup.selectAll('circle')
+        .data(points)
+        .enter()
+        .append('circle')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', 7)
+        .attr('fill', 'rgb(212, 198, 198)');
+
+        // Ensure the circle group is brought to the front
+        svg.node().appendChild(circleGroup.node());
+}
+
+// Update the canvas with D3.js visualization
+function updateLines() {
+    // Clear lines from the canvas
+    lineGroup.selectAll('path').remove();
     
+    // No lines to draw if fewer than 2 points
+    if (points.length < 2) {
+        return;
+    }
+
     // Draw a path connecting the points
     const line = d3.line()
                    .x(d => d.x)
                    .y(d => d.y);
 
-    svg.append('path')
+    lineGroup.append('path')
        .data([points])
        .attr('d', line)
        .attr('fill', 'none')
        .attr('stroke', 'rgb(95, 85, 205)')
        .attr('stroke-width', 2.5);
-
-    // Create circles for each point
-    svg.selectAll('circle')
-       .data(points)
-       .enter()
-       .append('circle')
-       .attr('cx', d => d.x)
-       .attr('cy', d => d.y)
-       .attr('r', 7)
-       .attr('fill', 'rgb(212, 198, 198)');
-
 }
